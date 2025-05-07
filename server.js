9e4e9be5cc5071cc1df8e5de1f578015b5e50d30
Bhusan <bhusan.dotel@nepalcancode.com>
2025-05-07 12:22:13 +0545

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/api/submit", (req, res) => {
+  const data = req.body;
+  console.log("Received data:", JSON.stringify(data, null, 2));
+  res.status(200).json({ message: "Data received successfully!" });
+});
+
+app.listen(3000, () => {
+  console.log("App listening on port 3000!");
+});
