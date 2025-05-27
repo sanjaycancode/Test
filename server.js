69263f2f48bfa71c6e9ec5873771cff65243cf74
Sanjay <sanzaikc@gmail.com>
2025-05-27 10:40:27 +0545

    Refactor webhook endpoint to improve structure and logging

--- server.js
+++ server.js
@@ -8,62 +8,68 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post("/outlook-webhook", (req, res) => {
-  if (req.query && req.query.validationToken) {
-    // Important: Set content type to text/plain
-    res.set("Content-Type", "text/plain");
+app.post("/webhook", (req, res) => {
+  // if (req.query && req.query.validationToken) {
+  //   // Important: Set content type to text/plain
+  //   res.set("Content-Type", "text/plain");
 
-    // Send back the exact same validationToken with 200 OK status
-    res.status(200).send(req.query.validationToken);
+  //   // Send back the exact same validationToken with 200 OK status
+  //   res.status(200).send(req.query.validationToken);
 
-    console.log("Validation response sent successfully");
-    return;
-  }
+  //   console.log("Validation response sent successfully");
+  //   return;
+  // }
 
-  const notificationData = req?.body?.value || null;
+  // const notificationData = req?.body?.value || null;
 
-  console.log({ notificationData });
+  // console.log({ notificationData });
 
-  if (!notificationData?.length)
-    return res.status(400).send("Invalid Notification Data!");
+  // if (!notificationData?.length)
+  //   return res.status(400).send("Invalid Notification Data!");
 
-  const messageResourceData = notificationData?.find(
-    (n) => n?.resourceData?.["@odata.type"] === "#Microsoft.Graph.Message"
-  );
+  // const messageResourceData = notificationData?.find(
+  //   (n) => n?.resourceData?.["@odata.type"] === "#Microsoft.Graph.Message"
+  // );
 
-  console.log({ messageResourceData });
+  // console.log({ messageResourceData });
 
-  res.status(200).send("Notification Received!");
+  // res.status(200).send("Notification Received!");
+
+  const data = req.body;
+
+  console.log(JSON.stringify(data, null, 2));
+
+  res.status(200).send("Data Received!");
 });
 
-app.post("/outlook-webhook/lifecycle", async (req, res) => {
-  try {
-    if (req.query && req.query.validationToken) {
-      // Important: Set content type to text/plain
-      res.set("Content-Type", "text/plain");
+// app.post("/webhook/lifecycle", async (req, res) => {
+//   try {
+//     if (req.query && req.query.validationToken) {
+//       // Important: Set content type to text/plain
+//       res.set("Content-Type", "text/plain");
 
-      // Send back the exact same validationToken with 200 OK status
-      res.status(200).send(req.query.validationToken);
+//       // Send back the exact same validationToken with 200 OK status
+//       res.status(200).send(req.query.validationToken);
 
-      console.log("Validation response sent successfully");
-      return;
-    }
+//       console.log("Validation response sent successfully");
+//       return;
+//     }
 
-    const data = req?.body?.value;
+//     const data = req?.body?.value;
 
-    if (!data) return res.status(400).json("No Data!");
+//     if (!data) return res.status(400).json("No Data!");
 
-    if (!data?.length) return res.status(500).json("Invalid Lifecycle Data!");
+//     if (!data?.length) return res.status(500).json("Invalid Lifecycle Data!");
 
-    const reauthorizationCycle = data?.find(
-      (c) => c?.lifecycleEvent === "reauthorizationRequired"
-    );
+//     const reauthorizationCycle = data?.find(
+//       (c) => c?.lifecycleEvent === "reauthorizationRequired"
+//     );
 
-    console.log("Reauthorization Notification", reauthorizationCycle);
-  } catch (error) {
-    console.log({ error });
-  }
-});
+//     console.log("Reauthorization Notification", reauthorizationCycle);
+//   } catch (error) {
+//     console.log({ error });
+//   }
+// });
 
 app.listen(3000, () => {
   console.log("App listening on port 3000!");
